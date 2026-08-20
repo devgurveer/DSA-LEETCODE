a65df8eb3116525392dfec246e07da871e8dc55a
gurveersingh <gurveer2456@gmail.com>
2026-08-20 20:15:23 +0530

    feat: update LeetCode 189. Rotate Array solution (cpp)

--- problems/rotate-array/solution.cpp
+++ problems/rotate-array/solution.cpp
@@ -3,13 +3,9 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
         k = k % n;
-        while (k > 0) {
-            int temp = nums[n - 1];
-            for (int i = n - 1; i > 0; i--) {
-                nums[i] = nums[i - 1];
-            }
-            nums[0] = temp;
-            k--;
-        }
+
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
     }
 };
